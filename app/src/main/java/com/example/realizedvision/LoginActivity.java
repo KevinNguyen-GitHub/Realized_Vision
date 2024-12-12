e8c95e105f4baef8793449173f627796fc4bc6f8
hsharma8757 <hritiksharma1234567@gmail.com>
2024-12-12 09:56:02 -0800

    fixed error that would take u straight to home page instead of login page first. Added back button on general settings page

--- app/src/main/java/com/example/realizedvision/LoginActivity.java
+++ app/src/main/java/com/example/realizedvision/LoginActivity.java
@@ -29,17 +29,7 @@ public class LoginActivity extends AppCompatActivity {
     FirebaseAuth mAuth;
     TextView textViewRegister;
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
-            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-            startActivity(intent);
-            finish();
-        }
-    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

26aebcf878ddfe26fbb743b66fa2b0507b11de8e
f-andrade27 <119544115+f-andrade27@users.noreply.github.com>
2024-12-11 22:09:50 -0800

    Added back buttons and linked pages

--- app/src/main/java/com/example/realizedvision/NotificationsActivity.java
+++ app/src/main/java/com/example/realizedvision/NotificationsActivity.java
@@ -1,4 +1,23 @@
 package com.example.realizedvision;
 
-public class NotificationsActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NotificationsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notifications);
+        ImageButton backButton = findViewById(R.id.backButtonNotification);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
 }

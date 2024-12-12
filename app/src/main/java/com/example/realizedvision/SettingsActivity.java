69b2b853d55d35b8cc3d344d8cd51ff94d82d876
hsharma8757 <hritiksharma1234567@gmail.com>
2024-12-12 09:56:02 -0800

    fixed error that would take u straight to home page instead of login page first. Added back button on general settings page

--- app/src/main/java/com/example/realizedvision/SettingsActivity.java
+++ app/src/main/java/com/example/realizedvision/SettingsActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -46,6 +47,16 @@ public class SettingsActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        ImageButton backButton = findViewById(R.id.backButtonChangePass);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingsActivity.this, ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
 
     }

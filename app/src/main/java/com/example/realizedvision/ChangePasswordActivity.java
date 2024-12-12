4a3c06b83a241d83588f22e3e1e732bf654a885c
hrsharma155 <154959113+hrsharma155@users.noreply.github.com>
2024-12-12 11:07:48 -0800

    Update ChangePasswordActivity.java

--- app/src/main/java/com/example/realizedvision/ChangePasswordActivity.java
+++ app/src/main/java/com/example/realizedvision/ChangePasswordActivity.java
@@ -105,26 +105,3 @@ public class ChangePasswordActivity extends AppCompatActivity {
 
 
 
-//package com.example.realizedvision;
-//
-//import android.os.Bundle;
-//import android.view.View;
-//import android.widget.ImageButton;
-//
-//import androidx.appcompat.app.AppCompatActivity;
-//
-//public class ChangePasswordActivity extends AppCompatActivity {
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_changepassword);
-//        ImageButton backButton = findViewById(R.id.backButtonChangePass);
-//        backButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                finish();
-//            }
-//        });
-//    }
-//}

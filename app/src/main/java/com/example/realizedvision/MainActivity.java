05f943291aa7109259c44571fd58e305578f9357
davidramirez116 <david.ramirez11@student.csulb.edu>
2024-12-12 00:53:32 -0800

    Updated MainActivity UI, added navigation to other activities

--- app/src/main/java/com/example/realizedvision/MainActivity.java
+++ app/src/main/java/com/example/realizedvision/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,61 +13,26 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tvWelcomeMessage;
-
     FirebaseAuth auth;
     FirebaseUser user;
-    Button buttonLogout;
-    Button buttonProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Initialize views
-        tvWelcomeMessage = findViewById(R.id.textView2);
-        auth = FirebaseAuth.getInstance();
-        buttonLogout = findViewById(R.id.btn_logout);
-        buttonProfile = findViewById(R.id.btn_profile);
-        user = auth.getCurrentUser();
+        ImageView favoriteIcon = findViewById(R.id.favorites_icon);
+        ImageView messageIcon = findViewById(R.id.messages_icon);
+        ImageView profileIcon = findViewById(R.id.profile_icon);
 
-        if (user == null) {
-            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-            startActivity(intent);
-            finish();
-        }
+        favoriteIcon.setOnClickListener(view -> navigateTo(FavoritesActivity.class));
+        messageIcon.setOnClickListener(view -> navigateTo(MessagesActivity.class));
+        profileIcon.setOnClickListener(view -> navigateTo(ProfileActivity.class));
 
-        // Get data from intent
-        String firstName = getIntent().getStringExtra("firstName");
-        String lastName = getIntent().getStringExtra("lastName");
-
-        // Set welcome message
-        if (firstName != null && lastName != null) {
-            tvWelcomeMessage.setText("Welcome, " + firstName + " " + lastName + "!");
-        } else {
-            tvWelcomeMessage.setText("Welcome!");
-        }
-
-        buttonLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
-
-        buttonProfile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
-                startActivity(intent);
-            }
-        });
-
-
-
+    }
+    //   Helper function for activity navigation
+    private void navigateTo(Class<?> targetActivity) {
+        Intent intent = new Intent(MainActivity.this, targetActivity);
+        startActivity(intent);
     }
 }

66000279871df1edaf34a9400a94839e05ef5070
f-andrade27 <frankieandrade1127@gmail.com>
2025-01-28 17:51:36 -0800

    added some item parts of  vendor page
    added item, item adapter, item view, and  vendor profile. added recycle view in dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,6 +70,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.material)
+    implementation(libs.androidx.recyclerview)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 

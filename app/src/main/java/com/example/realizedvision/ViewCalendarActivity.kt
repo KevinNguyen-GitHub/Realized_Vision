b5ec483a860537e8eb948ea2abe0ded1236c5317
f-andrade27 <119544115+f-andrade27@users.noreply.github.com>
2025-02-17 13:30:43 -0800

    reverted some files

--- app/src/main/java/com/example/realizedvision/ViewCalendarActivity.kt
+++ app/src/main/java/com/example/realizedvision/ViewCalendarActivity.kt
@@ -1,46 +1,15 @@
 package com.example.realizedvision
 
 
-import android.app.Activity
-import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
-import android.provider.Settings
-import android.util.Log
 import android.view.Gravity
 import android.view.View
-import android.widget.Button
-import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
-import android.widget.Toast
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.activity.result.launch
-import androidx.compose.ui.platform.ComposeView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.core.content.ContextCompat
-import androidx.credentials.Credential
-import androidx.credentials.CredentialManager
-import androidx.credentials.CustomCredential
-import androidx.credentials.GetCredentialRequest
-import androidx.credentials.GetCredentialResponse
-import androidx.credentials.exceptions.GetCredentialException
-import androidx.credentials.exceptions.NoCredentialException
-import androidx.lifecycle.lifecycleScope
-import com.google.android.libraries.identity.googleid.GetGoogleIdOption
-import com.google.android.libraries.identity.googleid.GetSignInWithGoogleOption
-import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
-import com.google.android.libraries.identity.googleid.GoogleIdTokenParsingException
-import com.google.firebase.auth.FirebaseAuth
 import com.kizitonwose.calendar.core.CalendarDay
 import com.kizitonwose.calendar.core.CalendarMonth
 import com.kizitonwose.calendar.core.DayPosition
@@ -48,16 +17,12 @@ import com.kizitonwose.calendar.view.CalendarView
 import com.kizitonwose.calendar.view.MonthDayBinder
 import com.kizitonwose.calendar.view.MonthHeaderFooterBinder
 import com.kizitonwose.calendar.view.MonthScrollListener
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
-import java.security.MessageDigest
 import java.time.DayOfWeek
 import java.time.LocalDate
 import java.time.YearMonth
 import java.time.format.DateTimeFormatter
 import java.time.format.TextStyle
 import java.util.Locale
-import java.util.UUID
 
 
 class ViewCalendarActivity : AppCompatActivity() {
@@ -74,7 +39,7 @@ class ViewCalendarActivity : AppCompatActivity() {
         calendarView = findViewById(R.id.calendarView)
         monthText = findViewById(R.id.monthText)
 
-        //setupCalendar()
+        setupCalendar()
 
 
     }

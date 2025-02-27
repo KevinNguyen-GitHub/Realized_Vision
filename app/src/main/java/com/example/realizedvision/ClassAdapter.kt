4a703198df3ea0f100152f0972a94378bb9901be
f-andrade27 <119544115+f-andrade27@users.noreply.github.com>
2025-02-27 08:52:47 -0800

    added firestore functionality and the ability to add classes
    vendors can now add classes

--- app/src/main/java/com/example/realizedvision/ClassAdapter.kt
+++ app/src/main/java/com/example/realizedvision/ClassAdapter.kt
@@ -3,42 +3,39 @@ package com.example.realizedvision
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.TextView
+import androidx.compose.ui.semantics.text
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import java.text.DateFormat
+import java.text.SimpleDateFormat
 import java.util.Locale
 
-
-class ClassAdapter(private val classList: List<ClassInfo>) :
-    RecyclerView.Adapter<ClassAdapter.ClassViewHolder>() {
+class ClassAdapter : ListAdapter<ClassInfo, ClassAdapter.ClassViewHolder>(ClassDiffCallback()) {
 
     class ClassViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val titleTextView: TextView = itemView.findViewById(R.id.classDetailTitle)
         val descriptionTextView: TextView = itemView.findViewById(R.id.classDetailDescription)
-        val timeSlotTextView: TextView = itemView.findViewById(R.id.classDetailTimeSlot)
-        val reserveButton: Button = itemView.findViewById(R.id.reserveButton)
+        val timeslotTextView: TextView = itemView.findViewById(R.id.classDetailTimeSlot)
+
+        fun bind(classData: ClassInfo) {
+            titleTextView.text = classData.title
+            descriptionTextView.text = classData.description
+
+            val startTimeFormatted = classData.startTime
+            val endTimeFormatted = classData.endTime
+            val timeSlot = "$startTimeFormatted - $endTimeFormatted"
+            timeslotTextView.text = timeSlot
+
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ClassViewHolder {
-        val itemView = LayoutInflater.from(parent.context)
-            .inflate(R.layout.view_class, parent, false) // Inflate view_class.xml
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.view_class, parent, false)
         return ClassViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: ClassViewHolder, position: Int) {
-        val currentItem = classList[position]
-        holder.titleTextView.text = currentItem.title
-        holder.descriptionTextView.text = currentItem.description
-
-        holder.timeSlotTextView.text = "${currentItem.startTime} - ${currentItem.endTime}"
-
-
-        holder.reserveButton.setOnClickListener {
-            println("Reserved: ${currentItem.title}")
-        }
+        val classData = getItem(position)
+        holder.bind(classData)
     }
-
-
-    override fun getItemCount() = classList.size
 }

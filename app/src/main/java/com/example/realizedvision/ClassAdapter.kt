a64f10e6feb4d93977653e312263e5284228821b
f-andrade27 <119544115+f-andrade27@users.noreply.github.com>
2025-02-27 09:39:22 -0800

    started remove functionality
    not complete!

--- app/src/main/java/com/example/realizedvision/ClassAdapter.kt
+++ app/src/main/java/com/example/realizedvision/ClassAdapter.kt
@@ -12,7 +12,17 @@ import java.util.Locale
 
 class ClassAdapter : ListAdapter<ClassInfo, ClassAdapter.ClassViewHolder>(ClassDiffCallback()) {
 
-    class ClassViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    interface OnItemClickListener {
+        fun onItemClick(classInfo: ClassInfo)
+    }
+
+    private var listener: OnItemClickListener? = null
+
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        this.listener = listener
+    }
+
+    class ClassViewHolder(itemView: View, private val listener: OnItemClickListener?) : RecyclerView.ViewHolder(itemView) {
         val titleTextView: TextView = itemView.findViewById(R.id.classDetailTitle)
         val descriptionTextView: TextView = itemView.findViewById(R.id.classDetailDescription)
         val timeslotTextView: TextView = itemView.findViewById(R.id.classDetailTimeSlot)
@@ -20,6 +30,7 @@ class ClassAdapter : ListAdapter<ClassInfo, ClassAdapter.ClassViewHolder>(ClassD
         fun bind(classData: ClassInfo) {
             titleTextView.text = classData.title
             descriptionTextView.text = classData.description
+            itemView.tag = classData
 
             val startTimeFormatted = classData.startTime
             val endTimeFormatted = classData.endTime
@@ -27,11 +38,20 @@ class ClassAdapter : ListAdapter<ClassInfo, ClassAdapter.ClassViewHolder>(ClassD
             timeslotTextView.text = timeSlot
 
         }
+        init {
+            itemView.setOnClickListener {
+                val position = getBindingAdapterPosition()
+                if (position != RecyclerView.NO_POSITION) {
+                    val classData = itemView.tag as ClassInfo
+                    listener?.onItemClick(classData)
+                }
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ClassViewHolder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.view_class, parent, false)
-        return ClassViewHolder(itemView)
+        return ClassViewHolder(itemView, listener)
     }
 
     override fun onBindViewHolder(holder: ClassViewHolder, position: Int) {

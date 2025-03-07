256774fe39a7e345589d82bf81620dccc45b4b75
Ruben <r.bautista9076@yahoo.com>
2025-03-07 13:02:40 -0800

    Added Booking and Cancelation

--- app/src/main/java/com/example/realizedvision/EditAvailability.java
+++ app/src/main/java/com/example/realizedvision/EditAvailability.java
@@ -13,6 +13,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.SetOptions;
 import com.kizitonwose.calendar.core.CalendarDay;
@@ -26,9 +27,11 @@ import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Set;
 
 import kotlin.Unit;
@@ -64,15 +67,9 @@ public class EditAvailability extends AppCompatActivity {
 
         setupCalendar();
 
-        calendarView.setMonthScrollListener(calendarMonth -> {
-            monthTextView.setText(monthFormat.format(calendarMonth.getYearMonth()));
-            return Unit.INSTANCE;
-        });
-
-
         backButton.setOnClickListener(view -> navigateTo(ViewCalendarActivity.class));
 
-        btnSave.setOnClickListener(v -> saveAvailabilityToDatabase());
+        btnSave.setOnClickListener(v -> saveAvailabilities());
     }
 
     private void setupCalendar() {
@@ -82,6 +79,12 @@ public class EditAvailability extends AppCompatActivity {
         calendarView.setup(currentMonth, endMonth, DayOfWeek.SUNDAY);
 
         monthTextView.setText(monthFormat.format(currentMonth));
+
+        // Scroll through Calendar
+        calendarView.setMonthScrollListener(calendarMonth -> {
+            monthTextView.setText(monthFormat.format(calendarMonth.getYearMonth()));
+            return Unit.INSTANCE;
+        });
 
         calendarView.setDayBinder(new MonthDayBinder<DayViewContainer>() {
             @Override
@@ -93,14 +96,14 @@ public class EditAvailability extends AppCompatActivity {
             public void bind(DayViewContainer container, CalendarDay day) {
                 container.getTextView().setText(String.valueOf(day.getDate().getDayOfMonth()));
 
-                // Highlight selected dates
+                // Sets color of date depending on status
                 if (selectedDates.contains(day.getDate())) {
                     container.getTextView().setBackgroundResource(R.drawable.solid_circle);
                 } else {
                     container.getTextView().setBackground(null);
                 }
 
-                // OnClick: Toggle selection
+                // Select or unselect a date on calendar UI
                 container.getTextView().setOnClickListener(v -> {
                     if (selectedDates.contains(day.getDate())) {
                         selectedDates.remove(day.getDate());
@@ -113,41 +116,48 @@ public class EditAvailability extends AppCompatActivity {
             }
         });
 
-        loadAvailabilityFromDatabase();
+        loadAvailabilities();
     }
 
-    private void saveAvailabilityToDatabase() {
+    private void saveAvailabilities() {
         if (currentUser == null) {
             Toast.makeText(this, "User not logged in", Toast.LENGTH_SHORT).show();
             return;
         }
+        // Creates array and converts selected dates on UI to string
         List<String> formattedDates = new ArrayList<>();
         for (LocalDate date : selectedDates) {
-            formattedDates.add(date.toString()); // Convert to String
+            formattedDates.add(date.toString());
         }
         String userID = currentUser.getUid();
+
+        // Saves array to DB
         firestore.collection("Vendors").document(userID)
                 .set(Collections.singletonMap("Availabilities", formattedDates), SetOptions.merge())
                 .addOnSuccessListener(aVoid -> Toast.makeText(this, "Availability Saved!", Toast.LENGTH_SHORT).show())
                 .addOnFailureListener(e -> Log.e("Calendar", "Error saving dates"));
     }
 
-    private void loadAvailabilityFromDatabase() {
+    private void loadAvailabilities() {
         if (currentUser == null) {
             Toast.makeText(this, "User not logged in", Toast.LENGTH_SHORT).show();
             return;
         }
         String userID = currentUser.getUid();
+
+        // Queries DB for availabilties as listed by vendor
         firestore.collection("Vendors").document(userID)
                 .get()
                 .addOnSuccessListener(documentSnapshot -> {
                     if (documentSnapshot.exists()) {
+                        // Gets stored array from DB
                         List<String> availableDates = (List<String>) documentSnapshot.get("Availabilities");
                         if (availableDates != null) {
                             for (String dateStr : availableDates) {
                                 selectedDates.add(LocalDate.parse(dateStr));
                             }
-                            calendarView.notifyCalendarChanged(); // Refresh UI
+                            // Updates Calendar
+                            calendarView.notifyCalendarChanged();
                         }
                     }
                 })
